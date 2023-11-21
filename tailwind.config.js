064c1992f9ffad6a5ac1ada9e377fd43b54b498e
LinThit27 <linthit2745@gmail.com>
2023-11-21 11:33:39 +0630

    config tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#000000",
+        secondary: "#1E95C8",
+        sidebar: "#002C32",
+        bluemark: "#1C78E4",
+        sidemenu: "#FFFFFF",
+        activeside: "#3CD5E9",
+        purchase: "#FFB422",
+        navbrand: "#1E95C8",
+      },
+    },
   },
   plugins: [],
 };
