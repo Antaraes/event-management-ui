4316e468964ef7c5932e25a71382156e12e6a182
LinThit27 <linthit2745@gmail.com>
2023-11-23 15:23:52 +0630

    added dynamic fetching to organizer dashboard chart

--- src/api/index.js
+++ src/api/index.js
@@ -9,3 +9,7 @@ export const getOrganizerProfile = (organizerId) =>
   API.get(`/organizer/${organizerId}`);
 export const updateOrganizerProfile = (organizerId, organizerData) =>
   API.patch(`/organizer/update/${organizerId}`, organizerData);
+export const getOrganizerDashboardBarChartData = (organizerId) =>
+  API.get(`/organizer-dashboard/barchart/${organizerId}`);
+export const getOrganizerDashboardOverviewData = (organizerId) =>
+  API.get(`/organizer-dashboard/overview-data/${organizerId}`);
