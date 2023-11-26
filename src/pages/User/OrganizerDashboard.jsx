fc4ccfbdc4894e4b3cd92aadaac2763960015527
LinThit27 <linthit2745@gmail.com>
2023-11-26 23:05:44 +0630

    added back button and title for each event dashboard

--- src/pages/User/OrganizerDashboard.jsx
+++ src/pages/User/OrganizerDashboard.jsx
@@ -10,13 +10,14 @@ import {
   getOrganizerDashboardOverviewData,
   getEventsByOrganizerId,
 } from "../../api/index";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import EventsByOrganizer from "../../components/Organizer/EventsByOrganizer";
 
 const OrganizerDashboard = () => {
   const location = useLocation();
   const { organizerId } = useParams();
   const [queryParams, setQueryParams] = useState("");
+  const navigate = useNavigate();
 
   //init query keys
   const [barDataQueryKey, setBarDataQueryKey] = useState([
@@ -41,10 +42,9 @@ const OrganizerDashboard = () => {
     getOrganizerDashboardOverviewData(organizerId, queryParams)
   );
 
+  const searchParams = new URLSearchParams(location.search);
+  const eventValue = searchParams.get("event");
   useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const eventValue = searchParams.get("event");
-
     if (eventValue !== null) {
       setQueryParams(`event=${eventValue}`);
 
@@ -73,21 +73,12 @@ const OrganizerDashboard = () => {
     }
   }, [location.search]);
 
-  // const pieChartData = [
-  //   ["Color", "Tickets"],
-  //   ["Kpay", 20],
-  //   ["Wave", 15],
-  //   ["A+", 3],
-  //   ["AYA Pay", 5],
-  // ];
-
-  // const lineData = [
-  //   ["Week", "Sales", "Expenses"],
-  //   ["week1", 1000, 400],
-  //   ["week2", 1170, 460],
-  //   ["week3", 660, 1120],
-  //   ["week4", 1030, 540],
-  // ];
+  const handleBackClick = () => {
+    setQueryParams("");
+    setBarDataQueryKey(["organizer-dashboard-bardata", organizerId]);
+    setOverviewDataQueryKey(["organizer-dashboard-overview", organizerId]);
+    navigate(`/organizer/dashboard/${organizerId}`);
+  };
 
   return (
     <>
@@ -97,8 +88,11 @@ const OrganizerDashboard = () => {
             Organizer Dashboard{" "}
             {allEventsByOrganizer &&
               queryParams &&
-              `(${allEventsByOrganizer[0].name})`}
+              allEventsByOrganizer.map((event) =>
+                event._id === eventValue ? `(${event.name})` : ""
+              )}
           </h1>
+
           <Link to="/create-event">
             <span className="px-3 py-2 rounded bg-green-400 font-semibold">
               Create event
@@ -106,6 +100,33 @@ const OrganizerDashboard = () => {
           </Link>
         </div>
         <div className="bg-white rounded-2xl text-primary grid grid-cols-1 md:grid-cols-2 p-5 md:p-8 mx-auto w-[90%] md:w-[85%] border-2 border-gray-900 min-h-[90vh] max-h-fit mb-5">
+          {queryParams && (
+            <span
+              onClick={handleBackClick}
+              className="flex w-fit p-1 hover:text-blue-700 gap-2 items-center hover:border-b border-blue-700 hover:-translate-y-1 duration-300 transition-all cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+                />
+              </svg>
+
+              <span className=" hidden sm:block">
+                {" "}
+                Back to overall dashboard{" "}
+              </span>
+            </span>
+          )}
+
           <div className="text-black h-fit col-span-2 md:w-full">
             {overviewData && (
               <OverviewBlock overviewData={overviewData} key={overviewData} />
