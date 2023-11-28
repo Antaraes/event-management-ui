5a8ebdb883ef3cc95a9bf06d1239a38f3f299a52
YeYint3424 <xino3424@gmail.com>
2023-11-28 14:16:39 +0630

    [modify] navbar

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -11,8 +11,8 @@ const Navbar = () => {
   return (
     <div className="bg-glass-transparent z-50 w-full flex justify-between fixed">
       <div className="h-10 w-[120px] sm:w-[145px] bg-navbrand relative px-3 rounded-br-3xl overflow-hidden flex">
-        <p className="h-full pt-[5px] sm:text-lg md:text-xl font-bold text-sidemenu cursor-pointer"><NavLink to="/">EVENTHUB</NavLink></p>
-        <span className="h-20 w-[30px] bg-sidebar absolute rotate-45 top-0 right-0"></span>
+        <p className="h-full pt-[5px] sm:text-lg md:text-xl font-bold text-sidemenu cursor-pointer z-50"><NavLink to="/">EVENTHUB</NavLink></p>
+        <span className="h-[80px] w-[120px] bg-sidebar absolute -rotate-[62deg] top-0 -right-[77px]"></span>
       </div>
       <div className="h-10 w-[150px] sm:w-[200px] bg-navbrand relative px-3 rounded-bl-3xl overflow-hidden flex">
         <p className="h-full pt-[10px] sm:pt-[5px] text-[0.8rem] sm:text-[1.2rem] text-sidemenu ml-5">
@@ -21,7 +21,7 @@ const Navbar = () => {
         <p className="h-full pt-[10px] sm:pt-[5px] text-[0.8rem] sm:text-[1.2rem] text-sidemenu ml-5 cursor-pointer z-50" onClick={()=>dispatch(setDrawer())}>
           Menu
         </p>
-        <span className="h-[80px] w-[120px] bg-sidebar absolute -rotate-[62deg] top-0 -right-[77px] "></span>
+        <span className="h-[80px] w-[120px] bg-sidebar absolute -rotate-[62deg] top-0 -right-[77px]"></span>
       </div>
       {/* <Sidebar /> */}
     </div>
