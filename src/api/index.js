5808b2c036caa7e4057a94ead313f6006c968499
LinnHtet29 <linnh3064@gmail.com>
2023-11-29 10:18:07 +0630

    [modify] fix bugs

--- src/api/index.js
+++ src/api/index.js
@@ -41,7 +41,7 @@ export const updateOrganizerPayment = (paymentId, paymentData) =>
   API.put(`/organizer-payment/update/${paymentId}`, paymentData);
 
 //Event
-export const getEvents = (query) => API.get(`/event/${query}`);
+export const getEvents = (query) => API.get(`/event${query}`);
 export const getEventById = (eventId) => API.get(`/event/find/${eventId}`);
 export const searchEvents = (query) => API.get(`/event/search${query}`);
 export const sortEvents = (query) => API.get(`/event/sort${query}`);
