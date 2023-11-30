43ace2ae9a9736de6bf84d9a54ccdd75a92e5436
LinThit27 <linthit2745@gmail.com>
2023-11-30 16:39:59 +0630

    pull from develop

--- src/components/Organizer/EventsByOrganizer.jsx
+++ src/components/Organizer/EventsByOrganizer.jsx
@@ -17,7 +17,7 @@ const EventsByOrganizer = ({ event }) => {
         {formatDate(event.eventStartDate)} - {formatDate(event.eventEndDate)}
       </span>
       <Link
-        to={`/organizer/dashboard/655db72a40abeabdf4678ec9?event=${event._id}`}
+        to={`/organizer/dashboard?event=${event._id}`}
         className="absolute right-2 bottom-2 hover:-translate-y-1 transition-all duration-300 hover:text-secondary hover:border-b hover:border-secondary"
       >
         See more Detail...
