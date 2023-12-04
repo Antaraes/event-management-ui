4c66acce20eb9e1b40a1ae91fe1944280c8896a9
LinThit27 <linthit2745@gmail.com>
2023-12-04 16:27:42 +0630

    [add] add  necessary package to package.json

--- src/Layout/AdminLayout.jsx
+++ src/Layout/AdminLayout.jsx
@@ -25,17 +25,19 @@ const AdminLayout = () => {
         }
       />
       <div className="p-4 xl:ml-80">
-        <DashboardNavbar />
-        {/* <Configurator /> */}
-        <IconButton
-          size="lg"
-          color="white"
-          className="fixed bottom-8 right-8 z-40 rounded-full shadow-blue-gray-900/10"
-          ripple={false}
-          onClick={() => setOpenConfigurator(dispatch, true)}
-        >
-          <Cog6ToothIcon className="h-5 w-5" />
-        </IconButton>
+        <div className="w-[600px]">
+          <DashboardNavbar />
+          <Configurator />
+          <IconButton
+            size="lg"
+            color="white"
+            className="fixed bottom-8 right-8 z-40 rounded-full shadow-blue-gray-900/10"
+            ripple={false}
+            onClick={() => setOpenConfigurator(dispatch, true)}
+          >
+            <Cog6ToothIcon className="h-5 w-5" />
+          </IconButton>
+        </div>
         <Outlet />
 
         <div className="text-blue-gray-600">
--- src/pages/Admin/Dashboard.jsx
+++ src/pages/Admin/Dashboard.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import { DataGridPro } from "@mui/x-data-grid-pro";
 import { useDemoData } from "@mui/x-data-grid-generator";
 const Dashboard = () => {
-  return <div className="h-screen">Dashbarod</div>;
+  const { data } = useDemoData({
+    dataSet: "Commodity",
+    rowLength: 10,
+    editable: true,
+  });
+  return (
+    <div className="w-[90vw]">
+      <DataGridPro
+        {...data}
+        loading={data.rows.length === 0}
+        rowHeight={38}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </div>
+  );
 };
 
 export default Dashboard;
